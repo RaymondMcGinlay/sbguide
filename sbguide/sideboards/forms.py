acdc152a425f8701ed24f793d47ea798014627db
Raymond McGinlay <ray@thisislevelup.com>
2019-04-28 23:53:31 +0100

    migrations for ownership and unicity checks

---
+++ sbguide/sideboards/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import SideboardItem, Sideboard
+
+class SideboardForm(ModelForm):
+    class Meta:
+        model = Sideboard
+        fields = ['deck', 'opponent']
+
+
+class SideboardItemForm(ModelForm):
+    class Meta:
+        model = SideboardItem
+        fields = ['card', 'delta', 'sideboard']
